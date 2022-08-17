77deb82cfd0d0658410e877e203f8a0ba9706424
Denis <mironov_dens@mail.ru>
2022-08-17 21:09:37 +0900

    добавлена Homeworks3. Решено ДЗ№3/1

---
+++ Homework3/HW008/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+// не понял, зачем нужно писать цикл.
+
+int a = new Random().Next(1, 11);
+int b = new Random().Next(1, 11);
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+double result = Math.Pow(a, b);
+Console.WriteLine(result);
