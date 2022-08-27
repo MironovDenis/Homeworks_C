fff5ecd16f50b98458bacd6936a18610d8575328
Denis <mironov_dens@mail.ru>
2022-08-27 16:24:32 +0900

    HW015 done.

---
+++ Homework5/HW015/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Write("Массив состоит из N элементов. Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[] NewArray()
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+string PrintArray(int[] array)
+{
+    string printarr = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        printarr = printarr + Convert.ToString(array[i]) + " ";
+    }
+    Console.WriteLine(printarr);
+    Console.WriteLine();
+    return printarr;
+}
+
+int Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    Console.WriteLine($"Сумма нечетных элементов массива = {sum}");
+    return sum;
+}
+
+int[] arr = NewArray();
+PrintArray(arr);
+Sum(arr);
